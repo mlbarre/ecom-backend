b701940a49c96704494e4fabcd944d1afcc07edf
mlb <mlbarrera1538@gmail.com>
2022-02-07 19:05:27 -0600

    added model code and just need to seed database to make sure running good

--- models/Product.js
+++ models/Product.js
@@ -10,14 +10,37 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
-  },
-  {
+    product_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
+  },
+}
 );
 
 module.exports = Product;
